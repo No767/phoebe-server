c4d1bbc6e6dabd510bdde643d3822756c1262859
diamondburned <diamond@arikawa-hi.me>
2024-02-02 22:24:42 -0800

    Add consts.py

---
+++ db/consts.py
@@ -0,0 +1,32 @@
+PRONOUNS = [
+    "he/him",
+    "she/her",
+    "they/them",
+    "it/its",
+    "xe/xem",
+    "ze/zir",
+    "other"
+]
+
+SEXUAL_ORIENTATIONS = [
+   "asexual",
+   "bisexual",
+   "demisexual",
+   "gay",
+   "heterosexual",
+   "lesbian",
+   "pansexual",
+   "queer",
+   "questioning",
+   "other"
+]
+
+GENDERS = [
+    "agender",
+    "androgynous",
+    "bigender",
+    "cisgender",
+    "genderfluid",
+    "genderqueer",
+    "nonbinary",
+]
