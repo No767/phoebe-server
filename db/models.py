eb5e7af750272bd57f058f441c8d928981111770
No767 <73260931+No767@users.noreply.github.com>
2024-02-02 22:55:34 -0800

    Implement DB models

--- db/models.py
+++ db/models.py
@@ -1,13 +1,57 @@
-from sqlmodel import Relationship, SQLModel, Field
+from sqlmodel import Field, ARRAY, SQLModel, create_engine, Column, Float, String, JSON
 from datetime import datetime
-
-
-class UserPassword(SQLModel, table=True):
-    id: Optional[int] = Field(foreign_key="public_user.id")
-    passhash: str
+from typing import Optional
+from enum import Enum
 
 
 class Session(SQLModel, table=True):
     token: str = Field(primary_key=True)
-    username: str = Field(foreign_key="user.username")
+    user_id: Optional[int] = Field(foreign_key="user.id")
     expires_at: datetime = Field(default_factory=datetime.utcnow)
+
+
+class User(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    bio: str
+    color: str
+    gender: str
+    nickname: str
+    preferred_name: str
+    pronouns: list[str] = Field(default=[], sa_column=Column(JSON))
+    sexual_orientations: list[str] = Field(default=[], sa_column=Column(JSON))
+    group_id: Optional[int] = Field(default=None, foreign_key="group.id")
+
+
+class UserPassword(SQLModel, table=True):
+    id: Optional[int] = Field(primary_key=True, foreign_key="user.id")
+    passhash: str
+
+
+class Group(SQLModel, table=True):
+    """
+    A group is a collection of users who live together.
+    It is not necessarily a house (think group of partners needing a housemate).
+    """
+
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str
+    bio: str
+    house_id: Optional[int] = Field(foreign_key="house.id")
+
+
+class House(SQLModel, table=True):
+    """
+    A house describes a living space for a group.
+    Its location is roughly accurate to the nearest city.
+    """
+
+    id: Optional[int] = Field(default=None, primary_key=True)
+    lat: float
+    lon: float
+
+
+class AccessLevel(int, Enum):
+    PUBLIC = 0
+    LEVEL1 = 1
+    LEVEL2 = 2
+    LEVEL3 = 3
