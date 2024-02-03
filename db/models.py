d2b9b42aa4580c8e91e90da01ffb2f95db47771c
diamondburned <diamond@arikawa-hi.me>
2024-02-02 23:02:38 -0800

    Fix db/models.py

--- db/models.py
+++ db/models.py
@@ -3,20 +3,18 @@ from datetime import datetime
 from typing import Optional
 
 
-# class UserPassword(SQLModel, table=True):
-#     # id: Optional[int] = Field(foreign_key="public_user.id")
-#     passhash: str
+class UserPassword(SQLModel, table=True):
+    id: Optional[int] = Field(primary_key=True, foreign_key="publicuser.id")
+    passhash: str
 
 
 class Session(SQLModel, table=True):
     token: str = Field(primary_key=True)
-    username: str = Field(foreign_key="user.username")
+    user_id: Optional[int] = Field(foreign_key="publicuser.id")
     expires_at: datetime = Field(default_factory=datetime.utcnow)
 
+
 class PublicUser(SQLModel, table=True):
-    
-    meta: dict = Field(default={}, sa_column=Column(JSON))
-    
     id: int = Field(primary_key=True)
     preferred_name: str
     # flairs: JSON
@@ -24,21 +22,11 @@ class PublicUser(SQLModel, table=True):
     bio: str
     # gender: str
     group_id: Optional[int] = Field(default=None, foreign_key="group.id")
-    
+
+
 class Group(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     name: str
     bio: str
     lat: float
     long: float
-    
-    
-    
-    
-    
-    # class Config:
-    #     arbitary_types_allowed = True
-    
-    
-    
-    
