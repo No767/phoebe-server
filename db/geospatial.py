9523baef428875bd91e4521a164b4a6efb6abd6a
diamondburned <diamond@arikawa-hi.me>
2024-02-03 13:18:32 -0800

    Implement db/geospatial.py

--- db/geospatial.py
+++ db/geospatial.py
@@ -1,13 +1,34 @@
 from db import Database
 from db.models import *
 from sqlmodel import select
+from haversine import haversine as calculate_distance
+import haversine
+import heapq
 
 
 async def search_nearest_houses(
     db: Database,
     lat: float,
     lon: float,
-    radius: float,
+    radius: float,  # in whatever unit the `unit` parameter is
+    unit=haversine.Unit.MILES,
+    limit=100,
 ) -> list[House]:
+    heap = []
     for house in await db.exec(select(House)):
-        house
+        distance = calculate_distance(
+            (lat, lon),
+            (house.lat, house.lon),
+            unit=unit,
+            normalize=True,
+        )
+        if distance <= radius:
+            continue
+
+        v = (-distance, house)
+        if len(heap) < limit:
+            heapq.heappush(heap, v)
+        else:
+            heapq.heappushpop(heap, v)
+
+    return [house for _, house in heapq.nlargest(limit, heap)]
