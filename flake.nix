84bc8ec197104e7afea2bad32218898f5f6b2f05
diamondburned <diamond@arikawa-hi.me>
2024-02-03 23:58:18 -0800

    Fix flake.nix for latest Poetry file

--- flake.nix
+++ flake.nix
@@ -19,6 +19,7 @@
 		flake-utils.lib.eachDefaultSystem (system:
 			let
 				pkgs = nixpkgs.legacyPackages.${system};
+
 				inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; })
 					mkPoetryApplication
 					defaultPoetryOverrides;
@@ -32,12 +33,22 @@
 							buildInputs = (old.buildInputs or [ ]) ++ (with super; [
 								setuptools
 								poetry
+								pdm-pep517
+								pdm-backend
+								truststore
 							]);
 						});
 					in
 						{
+							broadcaster = fixPoetryPackage super.broadcaster;
 							sonyflake-py = fixPoetryPackage super.sonyflake-py;
+							sse-starlette = fixPoetryPackage super.sse-starlette;
 							fastapi-pagination = fixPoetryPackage super.fastapi-pagination;
+							sphinxcontrib-jquery = super.sphinxcontrib-jquery.overridePythonAttrs (old: {
+								buildInputs = (old.buildInputs or [ ]) ++ (with super; [
+									sphinx
+								]);
+							});
 						}
 					);
 				};
